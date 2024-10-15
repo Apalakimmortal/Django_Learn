0de3b128ad08845505739307e1a91c53b34ffda1
Apalakimmortal <apalakimmortal045@gmail.com>
2024-10-15 13:30:17 +0530

    CRUD operations + integrated project

--- core/core/urls.py
+++ core/core/urls.py
@@ -17,11 +17,23 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from home.views import *
+from vege.views import *
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('',home,name="home"),
     path('success_page/',success_page,name="success_page"),
+    path('receipe/',receipe,name = "receipes"),
+    path('delete_receipe/<id>/',delete_receipe,name="delete_recepie"),
+    path('update_receipe/<id>/',update_receipe,name="update_recepie"),
     path('about/',about,name="abput"),
     path('contact/',contact,name="contact"),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+urlpatterns +=staticfiles_urlpatterns()
